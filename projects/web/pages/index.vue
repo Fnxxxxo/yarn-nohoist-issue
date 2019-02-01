12cfa6e0566939ab4ba49a7d549ff8e0fc85a00d
Jaron Long <jingyuan.long@linkedcare.cn>
2019-02-01 17:57:40 +0800

    Init issues.

--- projects/web/pages/index.vue
+++ projects/web/pages/index.vue
@@ -1,9 +1,9 @@
 <template>
   <section class="container">
     <div>
-      <logo />
+      <logo/>
       <h1 class="title">
-        web
+        {{title}}
       </h1>
       <h2 class="subtitle">
         My phenomenal Nuxt.js project
@@ -25,44 +25,48 @@
 </template>
 
 <script>
-import Logo from '~/components/Logo.vue'
+  import Logo from '~/components/Logo.vue'
+  import { mapState } from 'vuex'
 
-export default {
-  components: {
-    Logo
+  export default {
+    components: {
+      Logo
+    },
+    computed: {
+      ...mapState(['title'])
+    }
   }
-}
 </script>
 
 <style>
-.container {
-  margin: 0 auto;
-  min-height: 100vh;
-  display: flex;
-  justify-content: center;
-  align-items: center;
-  text-align: center;
-}
+  .container {
+    margin: 0 auto;
+    min-height: 100vh;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    text-align: center;
+  }
 
-.title {
-  font-family: 'Quicksand', 'Source Sans Pro', -apple-system, BlinkMacSystemFont,
+  .title {
+    font-family: 'Quicksand', 'Source Sans Pro', -apple-system, BlinkMacSystemFont,
     'Segoe UI', Roboto, 'Helvetica Neue', Arial, sans-serif;
-  display: block;
-  font-weight: 300;
-  font-size: 100px;
-  color: #35495e;
-  letter-spacing: 1px;
-}
+    display: block;
+    font-weight: 300;
+    font-size: 100px;
+    color: #35495e;
+    letter-spacing: 1px;
+  }
 
-.subtitle {
-  font-weight: 300;
-  font-size: 42px;
-  color: #526488;
-  word-spacing: 5px;
-  padding-bottom: 15px;
-}
+  .subtitle {
+    font-weight: 300;
+    font-size: 42px;
+    color: #526488;
+    word-spacing: 5px;
+    padding-bottom: 15px;
+  }
 
-.links {
-  padding-top: 15px;
-}
+  .links {
+    padding-top: 15px;
+  }
 </style>
